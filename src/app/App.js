8bab2f2523d88baba333b1f64204f9ae92245496
IgorSereginn <107862938+IgorSereginn@users.noreply.github.com>
2022-08-20 08:37:46 +0300

    Add files via upload

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navBar";
-import UsersList from "./layouts/users";
 import Main from "./layouts/main";
 import Login from "./layouts/login";
-import userPage from "./components/userPage";
+import Users from "./layouts/users";
 
 function App() {
     return (
     <div>
         <NavBar />
       <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/users" exact component={UsersList}/>
-        <Route path="/users:userId?" component={userPage} />
-        <Route path="/" exact component={Main}/>
+        <Route path="/" exact component={Main}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/users/:userId?" component={Users}></Route>
       </Switch>
        {/* <Users /> */}
    </div>
--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
-// import Users from "../layouts/users";
+import api from "../api";
+import QualitiesList from "./qualitiesList";
 
 const UserPage = ({ id }) => {
     const history = useHistory();
+    const [user, setUser] = useState();
+    useEffect(() => {
+        api.users.getById(id).then((data) => setUser(data));
+    }, []);
 
     const handleSave = () => {
         history.push("/users");
     };
-
-    return <>
-      (userId) {
-      <h1>{userId.name}</h1>
-}
+    if (user) {
+    return (
+        <>
+        <h1>{user.name}</h1>
+        <h2>{user.profession.name}</h2>
+        <QualitiesList qualities={user.qualities} />
+        <h3>Всего встреч: {user.completedMeetings}</h3>
+        <h3>Оценка: {user.rate}</h3>
         <button
             onClick= { () => {
                 handleSave();
@@ -21,9 +29,12 @@ const UserPage = ({ id }) => {
             }>
             Вернуться
         </button>
-    </>;
+    </>
+    );
+} else {
+    return <h1>Loading...</h1>;
+}
 };
-
 UserPage.propTypes = {
     id: PropTypes.string.isRequired
 };
