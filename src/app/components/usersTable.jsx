11a7b6c62a62731725bb504dea58cfc03696e2d1
IgorSereginn <107862938+IgorSereginn@users.noreply.github.com>
2022-08-20 08:40:03 +0300

    Add files via upload

---
+++ src/app/components/usersTable.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+import BookMark from "./bookmark";
+import QualitiesList from "./qualitiesList";
+import Table from "./table";
+
+const UserTable = ({
+    users,
+    onSort,
+    selectedSort,
+    onToggleBookMark,
+    onDelete,
+    ...rest
+}) => {
+    const columns = {
+        name: { name: "Имя", component: (user) => <Link to={`/users/${user._id}`}>{user.name}</Link> },
+        qualities: { name: "Качества", component: (user) => (<QualitiesList qualities={user.qualities}/>) },
+        professions: { path: "profession.name", name: "Профессия" },
+        completedMeetings: {
+            path: "completedMeetings",
+            name: "Встретился,раз"
+        },
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: {
+            path: "bookmark",
+            name: "Избранное",
+            component: (user) => (
+                <BookMark
+                    status={user.bookmark}
+                    onClick={() => onToggleBookMark(user._id)}
+        />
+            )
+        },
+        delete: {
+            component: (user) => (
+            <button
+                onClick={() => onDelete(user._id)}
+                className="btn btn-danger"
+        >
+            delete
+        </button>
+        )
+        }
+    };
+        return (
+            <Table
+                onSort = {onSort}
+                selectedSort = {selectedSort}
+                columns = {columns}
+                data = {users}
+            >
+            <TableHeader {...{ onSort, selectedSort, columns }}/>
+            <TableBody {...{ columns, data: users }}/>
+            </Table>
+    );
+};
+
+UserTable.propTypes = {
+    users: PropTypes.array.isRequired,
+    onSort: PropTypes.func.isRequired,
+    selectedSort: PropTypes.object.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
+};
+
+export default UserTable;
